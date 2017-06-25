f75b92a5ea52251a4c93fd3eb061f4ceb7878e2b
DPK\Deepak <Dpk@Thinkpad>
2017-06-25 23:36:59 +0530

    find longest common string in string array

---
+++ Algo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestCommonString
+{
+    public class Algo
+    {
+        public string Run(params string[] allStrings)
+        {
+            var smallest = FindSmallest(allStrings);
+            var result = string.Empty;
+            for (int i = smallest.Length; i > 0; i--)
+            {
+                foreach (var part in GetAllPossibleParts(smallest, i))
+                {
+                    if(allStrings.All(c=>c.IndexOf(part) > -1))
+                    {
+                        result = part;
+                        break;                        
+                    }
+                }
+
+                if(!string.IsNullOrEmpty(result))
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private List<string> GetAllPossibleParts(string smallString, int size)
+        {
+            var list = new List<string>();
+            for (int i = 0; i <= smallString.Length-size; i++)
+            {
+                list.Add(smallString.Substring(i, size));
+            }
+            return list;
+        }
+
+        private string FindSmallest(string[] allStrings)
+        {
+            return allStrings.OrderBy(c => c.Length).First();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestCommonString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowCount = 0;
+            
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("How many rows you can to enter?");
+                rowCount = int.Parse(Console.ReadLine());
+                args = new string[rowCount];
+                for (int i = 0; i < rowCount; i++)
+                {
+                    Console.WriteLine($"Enter Row number {i+1} - ");
+                    args[i] = Console.ReadLine();
+                }
+            }            
+            var obj = new Algo();
+            var commonText = obj.Run(args);
+
+            Console.WriteLine($"Longest Common text in {args.Length} lines is = '{commonText}'");
+            Console.ReadLine();
+        }
+    }
+}
